56d8ed6ee4ee211dd0552ccb1b3eb7831f415258
шол <dxgfx@fdxd.jh>
2021-02-23 20:02:50 +0600

    corrected

--- app/src/main/java/com/example/lesson1/MainActivity.java
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btn;
     private EditText eml, pass;
@@ -22,46 +23,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.btn);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        eml = findViewById(R.id.eml);
+        pass = findViewById(R.id.pass);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(this);
+
+        eml.addTextChangedListener(loginTextWatcher);
+        pass.addTextChangedListener(loginTextWatcher);
+    }
+
+    private TextWatcher loginTextWatcher;
+
+    {
+        loginTextWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
             @Override
-            public void onClick(View arg0) {
-
-                Toast.makeText(getApplicationContext(),
-                        eml.getText() + " Добро пожаловать", Toast.LENGTH_LONG).show();
-
-                eml = findViewById(R.id.eml);
-                pass = findViewById(R.id.pass);
-                btn = findViewById(R.id.btn);
-
-                eml.addTextChangedListener(loginTextWatcher);
-                pass.addTextChangedListener(loginTextWatcher);
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String emailInput = eml.getText().toString().trim();
+                String passwordInput = pass.getText().toString().trim();
+                btn.setEnabled(!emailInput.isEmpty() && !passwordInput.isEmpty());
             }
 
-            private TextWatcher loginTextWatcher;
-
-            {
-                loginTextWatcher = new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                    }
-
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-                        String emailInput = eml.getText().toString().trim();
-                        String passwordInput = pass.getText().toString().trim();
-
-                        btn.setEnabled(!emailInput.isEmpty() && !passwordInput.isEmpty());
-                    }
-
-                    @Override
-                    public void afterTextChanged(Editable s) {
-                    }
-                };
+            @Override
+            public void afterTextChanged(Editable s) {
             }
-        });
+        };
+    }
+
+    @Override
+    public void onClick(View v) {
+switch (v.getId()){
+    case R.id.btn:
+        Toast toast = Toast.makeText(getBaseContext(), eml.getText() + " добро пожаловать", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP, 400, 0);
+      toast.show();
+        break;
+}
     }
 }
 
